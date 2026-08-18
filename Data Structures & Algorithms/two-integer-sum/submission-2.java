1566d334715803a7e96089d593307fca4235c675
Matteo <46108618+matteoyon@users.noreply.github.com>
2026-08-18 07:41:30 +0200

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.java
@@ -0,0 +1,30 @@
+/*
+Input: array of numbers, target number
+Output int array, expected having two elements
+Constraints:
+arr: at least two elements, at most 10^3. numbers of array and terget lower bound 10^-7 and upper bound 10^7
+
+Constraints: given at most, 10^3 a O(n^2) solution is acceptable
+*/
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(target-nums[i],i);
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i]) && !map.get(nums[i]).equals(i)){
+                int[] output = {i,map.get(nums[i])};
+                return output;
+            }
+        }
+        return null;
+    }
+}
+
+/*
+[1,3,4,2]; 6
+
+{5,0}{3,1}{2,2}{4,3}
+*/
