0cae7d387c3669101ebdfd45d135035edd9b91a5
Matteo <46108618+matteoyon@users.noreply.github.com>
2026-09-04 20:24:01 +0200

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,32 @@
+class MinStack {
+
+    Deque<Integer> main;
+    Deque<Integer> min;
+
+    public MinStack() {
+        main = new ArrayDeque<Integer>();
+        min = new ArrayDeque<Integer>();
+    }
+    
+    public void push(int val) {
+        main.push(val);
+        if(min.peek() == null)
+            min.push(val);
+        else if(min.peek() >= val)
+            min.push(val);
+    }
+    
+    public void pop() {
+        if(main.peek().equals(min.peek()))
+            min.pop();
+        main.pop();
+    }
+    
+    public int top() {
+        return main.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
